24d13e47916295a3ae335feb4e1859381355746b
Christophe Simon <geektophe@gmail.com>
2022-02-08 15:45:23 +0100

    Replaced xfce4-clipman by parcellite

--- config.def.h
+++ config.def.h
@@ -130,7 +130,6 @@ static const char *termcmd[]       = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "175x45", NULL };
 static const char *scrlockcmd[]    = { "pkill", "-USR1", "xidle", NULL };
-static const char *clipmancmd[]    = { "xfce4-clipman-history", NULL };
 static const char *scrshotcmd[]    = { "screenshot", NULL };
 
 #ifdef __OpenBSD__
@@ -215,7 +214,6 @@ static Key keys[] = {
 	{ MODKEY,                       31,        togglescratch,  {.v = scratchpadcmd } },// I
 
 	{ ControlMask|Mod1Mask,         32,        spawn,          {.v = scrlockcmd} },    // O
-	{ ControlMask|Mod1Mask,         60,        spawn,          {.v = clipmancmd} },    // Slash
 
 #ifdef __OpenBSD__
 	{ 0,                            160,        spawn,          {.v = voltogglecmd} },
